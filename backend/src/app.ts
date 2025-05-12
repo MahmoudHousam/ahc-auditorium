ea82e671fa0be39ceab7760bba18701cfeabe71a
MahmoudHousam <mahmoudhousam60@gmail.com>
2025-05-12 19:10:36 +0300

    configure frontend/backend

---
+++ backend/src/app.ts
@@ -0,0 +1,22 @@
+import express from 'express'
+import cors from 'cors'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const app = express()
+const PORT = process.env.PORT || 5000
+
+app.use(cors({
+    origin: "https://localhsot:5173" // Vite port
+}))
+
+app.use(express.json())
+
+app.get("/api/health", (req, res) => {
+    res.json({status: "healthy"})
+})
+
+app.listen(PORT, () => {
+    console.log(`Serve is running on ${PORT}`)
+})
